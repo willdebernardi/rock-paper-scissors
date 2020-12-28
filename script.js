2da3dcf61a463139c6c91a99acb3b4f2ea3d4a23
Will DeBernardi <will@debernardi.us>
2020-12-27 17:08:38 -0700

    Added comments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function computerPlay() {
+    // Uses Math.random() to generate a number between 0-2
     let choice = Math.floor(Math.random() * 3);
+    // Reassigns string value depending on which number
     switch(choice) {
         case 0:
             choice = "Rock";
@@ -15,6 +17,8 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    // Very verbose win checking that I am not a fan of
+    // Makes selection case-insensitive with .toLowerCase()
     if (playerSelection.toLowerCase() == "rock" &&
     computerSelection.toLowerCase() == "rock") {
         return "tie";
@@ -49,25 +53,30 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+// Lets user decide to play again or not
 function playAgain() {
-    if (prompt("Would you like to play again?").toLowerCase() == "yes") {
+    if (prompt("Would you like to play again? (Yes/No)").toLowerCase() == "yes") {
         game();
     } else {
         alert("Thank you for playing! Have a good day.")
     }
 }
 
+// Main game loop
 function game() {
+    // Variables to keep track of win/loss and round number
     let round = 0;
     let playerWin = 0;
     let computerWin = 0;
     let tieRound = 0;
     console.log("Starting new game!");
+    // While loop that executes until round 5
     while (round <= 4) {
         var playerSelection = prompt("Rock, paper, or scissors?");
         var computerSelection = computerPlay();
         console.log("Computer chose: " + computerSelection);
         let result = playRound(playerSelection, computerSelection);
+        // Only advances round if there is not a tie
         if (result == "win") {
             playerWin++;
             console.log("Player: " + playerWin);
