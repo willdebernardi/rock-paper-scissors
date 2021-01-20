8bce2a27432b167bbd6c7e12a9d22c0d4b47cb18
Will DeBernardi <will.debernardi1@gmail.com>
2021-01-19 19:55:50 -0700

    Added buttons and logic to handle selection

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
+const rockButton = document.getElementById("rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+rockButton.addEventListener("click", function(){
+    let playerSelection = "rock";
+    let computerSelection = computerPlay();
+    playRound(playerSelection,computerSelection);
+});
+
+paperButton.addEventListener("click", function() {
+    let playerSelection = "paper";
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+});
+
+scissorsButton.addEventListener("click", function() {
+    let playerSelection = "scissors";
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+});
+
 function computerPlay() {
     // Uses Math.random() to generate a number between 0-2
     let choice = Math.floor(Math.random() * 3);
     // Reassigns string value depending on which number
     switch(choice) {
         case 0:
-            choice = "Rock";
+            choice = "rock";
             break;
         case 1:
-            choice = "Paper";
+            choice = "paper";
             break;
         case 2:
-            choice = "Scissors";
+            choice = "scissors";
             break; 
     }
     return choice;
@@ -19,36 +41,39 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     // Very verbose win checking that I am not a fan of
     // Makes selection case-insensitive with .toLowerCase()
-    if (playerSelection.toLowerCase() == "rock" &&
-    computerSelection.toLowerCase() == "rock") {
+    if (playerSelection == "rock" &&
+    computerSelection == "rock") {
+        console.log("tie");
         return "tie";
-    } else if (playerSelection.toLowerCase() == "rock" &&
-    computerSelection.toLowerCase() == "scissors") {
+    } else if (playerSelection == "rock" &&
+    computerSelection == "scissors") {
+        console.log("win");
         return "win";
-    } else if (playerSelection.toLowerCase() == "rock" &&
-    computerSelection.toLowerCase() == "paper") {
+    } else if (playerSelection == "rock" &&
+    computerSelection == "paper") {
+        console.log("lose");
         return "lose";
     }
 
-    if (playerSelection.toLowerCase() == "paper" &&
-    computerSelection.toLowerCase() == "rock") {
+    if (playerSelection == "paper" &&
+    computerSelection == "rock") {
         return "win";
-    } else if (playerSelection.toLowerCase() == "paper" &&
-    computerSelection.toLowerCase() == "paper") {
+    } else if (playerSelection == "paper" &&
+    computerSelection == "paper") {
         return "tie";
-    } else if (playerSelection.toLowerCase() == "paper" &&
-    computerSelection.toLowerCase() == "scissors") {
+    } else if (playerSelection == "paper" &&
+    computerSelection == "scissors") {
         return "lose";
     }
 
-    if(playerSelection.toLowerCase() == "scissors" &&
-    computerSelection.toLowerCase() == "rock") {
+    if(playerSelection == "scissors" &&
+    computerSelection == "rock") {
         return "lose";
-    } else if (playerSelection.toLowerCase() == "scissors" &&
-    computerSelection.toLowerCase() == "paper") {
+    } else if (playerSelection == "scissors" &&
+    computerSelection == "paper") {
         return "win";
-    } else if (playerSelection.toLowerCase() == "scissors" &&
-    computerSelection.toLowerCase() == "scissors") {
+    } else if (playerSelection == "scissors" &&
+    computerSelection == "scissors") {
         return "tie";
     }
 }
@@ -62,40 +87,40 @@ function playAgain() {
     }
 }
 
-// Main game loop
-function game() {
-    // Variables to keep track of win/loss and round number
-    let round = 0;
-    let playerWin = 0;
-    let computerWin = 0;
-    let tieRound = 0;
-    console.log("Starting new game!");
-    // While loop that executes until round 5
-    while (round <= 4) {
-        var playerSelection = prompt("Rock, paper, or scissors?");
-        var computerSelection = computerPlay();
-        console.log("Computer chose: " + computerSelection);
-        let result = playRound(playerSelection, computerSelection);
-        // Only advances round if there is not a tie
-        if (result == "win") {
-            playerWin++;
-            console.log("Player: " + playerWin);
-            round++;
-        } else if (result == "lose") {
-            computerWin++;
-            console.log("Computer: " + computerWin);
-            round++;
-        } else if (result =="tie") {
-            tieRound++;
-            console.log("Tie: " + tieRound);
-        }
-    }
-    if (playerWin > computerWin){
-        console.log("You won!")
-    } else if (computerWin > playerWin) {
-        console.log("You lost. :(")
-    }
-    playAgain();
-}
+// // Main game loop
+// function game() {
+//     // Variables to keep track of win/loss and round number
+//     let round = 0;
+//     let playerWin = 0;
+//     let computerWin = 0;
+//     let tieRound = 0;
+//     console.log("Starting new game!");
+//     // While loop that executes until round 5
+//     // while (round <= 4) {
+//     //     var playerSelection = prompt("Rock, paper, or scissors?");
+//     //     var computerSelection = computerPlay();
+//     //     console.log("Computer chose: " + computerSelection);
+//     //     let result = playRound(playerSelection, computerSelection);
+//     //     // Only advances round if there is not a tie
+//     //     if (result == "win") {
+//     //         playerWin++;
+//     //         console.log("Player: " + playerWin);
+//     //         round++;
+//     //     } else if (result == "lose") {
+//     //         computerWin++;
+//     //         console.log("Computer: " + computerWin);
+//     //         round++;
+//     //     } else if (result =="tie") {
+//     //         tieRound++;
+//     //         console.log("Tie: " + tieRound);
+//     //     }
+//     // }
+//     if (playerWin > computerWin){
+//         console.log("You won!")
+//     } else if (computerWin > playerWin) {
+//         console.log("You lost. :(")
+//     }
+//     // playAgain();
+// }
 
-game();
+// game();
